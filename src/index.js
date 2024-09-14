2d56bc99a5a678ce5ef0d45e8e82b93184ad4fd5
kl <kanglu-email@qq.com>
2024-09-14 15:13:53 +0800

    feat(package.json): 更新

--- src/index.js
+++ src/index.js
@@ -15,26 +15,29 @@ commander.program.version(
 ).parse(process.argv)
 
 /**
- * 
  * init 项目 
  */ 
 commander.program.arguments('<dir>')  // [] 可填  <>必填
 .description('请输入项目名称！')
-.action((dir) => {
-    return inquirer.default.prompt([
-        {
-            type:'list',
-            name:'framework',
-            message:'which framework do you like?',
-            choices:['vue']
-        }
-    ]).then(() => {
-        // fs.writeFileSync()  区分不同选择，创建不同文件，写入不同内容，
-        // 实际 可以将这部分文件单独放在git上，即使安装最新cli也可以下载
+.action(async (dir) => {
+    // return inquirer.default.prompt([
+    //     {
+    //         type:'list',
+    //         name:'framework',
+    //         message:'which framework do you like?',
+    //         choices:['vue']
+    //     }
+    // ]).then(() => {
+    //     // fs.writeFileSync()  区分不同选择，创建不同文件，写入不同内容，
+    //     // 实际 可以将这部分文件单独放在git上，即使安装最新cli也可以下载
 
-        const fullDir = path.resolve(process.cwd(), dir)
-        const gitPath = 'https://github.com/kongbaiy/core-vue-template.git'
-        const command = `git clone -b develop ${gitPath} ${fullDir}`
-        childProcess.execSync(command)
-    })
-})
+    //     const fullDir = path.resolve(process.cwd(), dir)
+    //     const gitPath = 'https://github.com/kongbaiy/core-vue-template.git'
+    //     const command = `git clone -b develop ${gitPath} ${fullDir}`
+    //     childProcess.execSync(command)
+    // })
+    const fullDir = path.resolve(process.cwd(), dir)
+    const gitPath = 'https://github.com/kongbaiy/core-vue-template.git'
+    const command = `git clone -b develop ${gitPath} ${fullDir}`
+    childProcess.execSync(command)
+}).parse(process.argv)
