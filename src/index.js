273ba2cb3c2f81ef12984a86dae990b8d3f73614
kl <kanglu-email@qq.com>
2024-09-14 17:15:06 +0800

    feat: 命令调整，新增 loading

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const path = require('path')
-
 const commander = require('commander')
 const package = require('../package.json')
 const figlet = require('figlet')
 const cliName = figlet.textSync('core-vue-cli')
-const inquirer = require('inquirer')
-const childProcess = require('child_process')
 const download = require('download-git-repo')
+const ora = require('ora')
+const spinner = ora('loading').start();
 
 /**
  * version 
@@ -18,39 +17,17 @@ commander.program.version(
 /**
  * init 项目 
  */ 
-commander.program.arguments('<dir>')  // [] 可填  <>必填
-.description('请输入项目名称！')
+commander.program
+.command('init <projectName>') // [] 可填  <>必填
+.description('monorepo core vue template')
 .action(async (dir) => {
-    // return inquirer.default.prompt([
-    //     {
-    //         type:'list',
-    //         name:'framework',
-    //         message:'which framework do you like?',
-    //         choices:['vue']
-    //     }
-    // ]).then(() => {
-    //     // fs.writeFileSync()  区分不同选择，创建不同文件，写入不同内容，
-    //     // 实际 可以将这部分文件单独放在git上，即使安装最新cli也可以下载
-
-    //     const fullDir = path.resolve(process.cwd(), dir)
-    //     const gitPath = 'https://github.com/kongbaiy/core-vue-template.git'
-    //     const command = `git clone -b develop ${gitPath} ${fullDir}`
-    //     childProcess.execSync(command)
-    // })
-
-    // const fullDir = path.resolve(process.cwd(), dir)
-    // const gitPath = 'https://github.com/kongbaiy/core-vue-template'
-    // const command = `git clone -b develop ${gitPath} ${fullDir}`
-    // childProcess.execSync(command)
-    
     const fullDir = path.resolve(process.cwd(), dir)
 
-    download(
-        'direct:https://github.com/kongbaiy/core-vue-template#develop', 
-        fullDir,
-        { clone: true  },
-        (error) => {
-        }
-    )
+    spinner.start()
+    download('direct:https://github.com/kongbaiy/core-vue-template#develop', fullDir, { clone: true  }, (error) => {
+        if (error) console.error('template download error!')
+        spinner.stop()
+    })
 
-}).parse(process.argv)
+})
+commander.program.parse(process.argv)
